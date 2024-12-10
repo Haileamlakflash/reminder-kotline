0bbff8aae7cf74b6fd37e717ee66d2eb2726df3b
Haileamlakflash <haliemandeseman@gmail.com>
2024-12-10 16:14:48 +0300

    Create KoinModule.kt

---
+++ app/src/main/java/com/elementary/tasks/calander/KoinModule.kt
@@ -0,0 +1,26 @@
+package com.elementary.tasks.calendar
+
+import com.elementary.tasks.calendar.data.CalendarDataEngine
+import com.elementary.tasks.calendar.data.CalendarDataEngineBroadcast
+import com.elementary.tasks.calendar.data.DayLiveData
+import com.elementary.tasks.calendar.data.MonthLiveData
+import com.elementary.tasks.calendar.dayview.WeekViewModel
+import com.elementary.tasks.calendar.dayview.day.DayViewModel
+import com.elementary.tasks.calendar.dayview.weekheader.WeekFactory
+import com.elementary.tasks.calendar.dayview.weekheader.WeekHeaderController
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val calendarModule = module {
+  factory { WeekFactory(get(), get()) }
+  factory { WeekHeaderController(get()) }
+
+  viewModel { WeekViewModel(get(), get()) }
+  factory { DayViewModel(get(), get(), get(), get(), get(), get()) }
+
+  single { CalendarDataEngine(get(), get(), get(), get(), get(), get(), get(), get()) }
+  factory { CalendarDataEngineBroadcast(get()) }
+
+  factory { MonthLiveData(get(), get(), get(), get(), get()) }
+  factory { DayLiveData(get(), get(), get(), get(), get()) }
+}
